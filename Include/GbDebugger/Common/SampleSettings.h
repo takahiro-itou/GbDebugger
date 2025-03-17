65e71258afb85ba08aceb34fba6de1b27e49f9f3
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-03-17 20:44:00 +0900

    リファクタリング
    (1) ファイル名を変更

--- Include/GbDebugger/Common/SampleSettings.h
+++ Include/GbDebugger/Common/SampleSettings.h
@@ -23,7 +23,7 @@
 
 //  スクリプトによる設定値が書き込まれたヘッダを読み込む。  //
 #if defined( SAMPLE_USE_PRE_CONFIGURED_MSVC )
-#    include    "GbDebugger/.Config/PreConfigSample.msvc.h"
+#    include    "GbDebugger/.Config/PreConfigDebugger.msvc.h"
 #else
 #    include    "GbDebugger/.Config/ConfiguredDebugger.h"
 #endif
