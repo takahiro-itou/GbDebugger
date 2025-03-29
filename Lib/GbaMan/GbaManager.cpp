a2feeff28a9591069bdf6fbc5043dd83810e46d0
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-03-29 18:19:22 +0900

    テーブルを定義

--- Lib/GbaMan/GbaManager.cpp
+++ Lib/GbaMan/GbaManager.cpp
@@ -29,6 +29,16 @@ namespace  GbaMan  {
 
 namespace  {
 
+const char * regs[16] = {
+    "R0" , "R1" , "R2" , "R3" , "R4" , "R5", "R6", "R7",
+    "R8" , "R9" , "R10", "R11", "R12", "SP", "LR", "PC"
+};
+
+const char * conditions[16] = {
+    "EQ", "NE", "CS", "CC", "MI", "PL", "VS", "VC",
+    "HI", "LS", "GE", "LT", "GT", "LE", "AL", "NV"
+};
+
 }   //  End of (Unnamed) namespace.
 
 
