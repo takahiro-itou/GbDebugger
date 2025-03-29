d17e822649efe4679e49755ba15327cde722e739
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-03-29 18:58:46 +0900

    実装作業途中

--- Lib/GbaMan/GbaManager.cpp
+++ Lib/GbaMan/GbaManager.cpp
@@ -20,6 +20,7 @@
 
 #include    "GbDebugger/GbaMan/GbaManager.h"
 
+#include    <ostream>
 #include    <stdio.h>
 #include    <sys/stat.h>
 
@@ -29,6 +30,12 @@ namespace  GbaMan  {
 
 namespace  {
 
+struct  Opecodes  {
+    uint32_t    mask;
+    uint32_t    cval;
+    const char *    mnemonic;
+};
+
 const char * regs[16] = {
     "R0" , "R1" , "R2" , "R3" , "R4" , "R5", "R6", "R7",
     "R8" , "R9" , "R10", "R11", "R12", "SP", "LR", "PC"
@@ -37,6 +44,13 @@ const char * regs[16] = {
 const char * conditions[16] = {
     "EQ", "NE", "CS", "CC", "MI", "PL", "VS", "VC",
     "HI", "LS", "GE", "LT", "GT", "LE", "AL", "NV"
+};
+
+const Opecodes armOpecodes[] = {
+    //  Branch
+    { 0x0F000000, 0x0A000000, "b%c %r0" },
+    { 0x0F000000, 0x0B000000, "bl%c %o" },
+    { 0x0F000000, 0x0F000000, "swi%c %q" }
 };
 
 }   //  End of (Unnamed) namespace.
@@ -136,7 +150,51 @@ GbaManager::disassembleArm(
         std::ostream  & outStr,
         const uint32_t  addr)
 {
+    char    buf[256];
     uint32_t    opecode = readMemory<uint32_t>(this->m_tblMem, addr);
+
+    const Opecodes * oc = armOpecodes;
+    for ( ; (opecode & oc->mask) != oc->cval; ++ oc ) ;
+
+    sprintf(buf, "%08x %08x ", addr, opecode);
+    outStr  <<  buf;
+
+    int             reg_id  = 0;
+    size_t          len = 0;
+    const  char  *  src = oc->mnemonic;
+    char  *         dst = buf;
+
+    while (*src) {
+        len = 0;
+        if ( *src != '%' ) {
+            *(dst ++)   = *(src ++);
+        } else {
+            ++  src;
+            switch ( *src ) {
+            case  'c':
+                len = sprintf(dst, "%s", conditions[opecode >> 28]);
+                break;
+            case  'r':
+                reg_id  = (opecode >> ((*(++ src) - '0') * 4)) & 15;
+                len = sprintf(dst, "%s", regs[reg_id]);
+                break;
+            case  'o':  {
+                * (dst ++)  = '$';
+                int off = opecode & 0x00FFFFFF;
+                if ( off & 0x00800000 ) {
+                    off |= 0xFF000000;
+                }
+                off <<= 2;
+                len = sprintf(dst, "%08x", addr + 8 + off);
+                }
+                break;
+            }
+        }
+        dst += len;
+    }
+
+    *(dst ++)   = '\0';
+    outStr  <<  buf;
 
     return ( outStr );
 }
