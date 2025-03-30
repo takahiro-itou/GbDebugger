bb1aeab7b830481653e0fb0b72aa63094e8bb4a5
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-03-30 10:44:49 +0900

    空のヘッダファイルを新規作成

---
+++ Lib/GbaMan/CpuArm/CpuArm.h
@@ -0,0 +1,38 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   Gb Debugger Project.   ----                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of CpuArm.
+**
+**      @file       GbaMan/CpuArm.h
+**/
+
+#if !defined( GBDEBUGGER_GBAMAN_INCLUDED_CPU_ARM_H )
+#    define   GBDEBUGGER_GBAMAN_INCLUDED_CPU_ARM_H
+
+#if !defined( GBDEBUGGER_COMMON_INCLUDED_DEBUGGER_TYPES_H )
+#    include    "GbDebugger/Common/DebuggerTypes.h"
+#endif
+
+
+GBDEBUGGER_NAMESPACE_BEGIN
+namespace  GbaMan  {
+
+//  クラスの前方宣言。  //
+
+
+}   //  End of namespace  GbaMan
+GBDEBUGGER_NAMESPACE_END
+
+#endif
