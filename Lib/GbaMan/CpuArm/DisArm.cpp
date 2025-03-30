86f39b3a2e30b1fc004a3de401fe909955b1e700
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-03-30 11:05:30 +0900

    空のソースファイルを新規作成

---
+++ Lib/GbaMan/CpuArm/DisArm.cpp
@@ -0,0 +1,36 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   Gb Debugger Project.   ----                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of DisArm class.
+**
+**      @file       GbaMan/DisArm.cpp
+**/
+
+#include    "DisArm.h"
+#include    "GbDebugger/GbaMan/GbaManager.h"
+
+#include    <ostream>
+
+
+GBDEBUGGER_NAMESPACE_BEGIN
+namespace  GbaMan  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace.
+
+
+}   //  End of namespace  GbaMan
+GBDEBUGGER_NAMESPACE_END
