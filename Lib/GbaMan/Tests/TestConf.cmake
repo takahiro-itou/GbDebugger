3ad745b7e256ff040cd70b29b6ab98b01cade026
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-04-03 19:28:15 +0900

    ファイルをビルド対象に追加

--- Lib/GbaMan/Tests/TestConf.cmake
+++ Lib/GbaMan/Tests/TestConf.cmake
@@ -8,9 +8,14 @@ add_test(NAME   GbaManagerTest
     COMMAND  $<TARGET_FILE:GbaManagerTest>
 )
 
+add_test(NAME   MemoryManagerTest
+    COMMAND  $<TARGET_FILE:MemoryManagerTest>
+)
+
 ##----------------------------------------------------------------
 ##
 ##    テストプログラムのビルド。
 ##
 
 add_executable(GbaManagerTest           GbaManagerTest.cpp)
+add_executable(MemoryManagerTest        MemoryManagerTest.cpp)
