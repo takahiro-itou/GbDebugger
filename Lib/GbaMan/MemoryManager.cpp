3a771ed843cff22e01c559b8edafe209ee0cf8b4
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-04-12 11:27:49 +0900

    メンバ関数を実装

--- Lib/GbaMan/MemoryManager.cpp
+++ Lib/GbaMan/MemoryManager.cpp
@@ -86,6 +86,46 @@ MemoryManager::~MemoryManager()
 //    Public Member Functions (Virtual Functions).
 //
 
+//----------------------------------------------------------------
+//    メモリを確保する。
+//
+
+ErrCode
+MemoryManager::allocateMemory()
+{
+    this->m_memBios = new BtByte [MEM_SIZE_BIOS];
+    this->m_memWRam = new BtByte [MEM_SIZE_WRAM];
+    this->m_memIRam = new BtByte [MEM_SIZE_IRAM];
+    this->m_memIO   = new BtByte [MEM_SIZE_IOMEM];
+    this->m_memPRam = new BtByte [MEM_SIZE_PRAM];
+    this->m_memVRam = new BtByte [MEM_SIZE_VRAM];
+    this->m_memOam  = new BtByte [MEM_SIZE_OAM];
+    this->m_memRom  = new BtByte [MEM_SIZE_ROM];
+    this->m_memSave = new BtByte [MEM_SIZE_SRAM];
+
+    return ( ErrCode::SUCCESS );
+}
+
+//----------------------------------------------------------------
+//    メモリを解放する。
+//
+
+ErrCode
+MemoryManager::releaseMemory()
+{
+    delete []   this->m_memBios;    this->m_memBios = nullptr;
+    delete []   this->m_memWRam;    this->m_memWRam = nullptr;
+    delete []   this->m_memIRam;    this->m_memIRam = nullptr;
+    delete []   this->m_memIO;      this->m_memIO   = nullptr;
+    delete []   this->m_memPRam;    this->m_memPRam = nullptr;
+    delete []   this->m_memVRam;    this->m_memVRam = nullptr;
+    delete []   this->m_memOam;     this->m_memOam  = nullptr;
+    delete []   this->m_memRom;     this->m_memRom  = nullptr;
+    delete []   this->m_memSave;    this->m_memSave = nullptr;
+
+    return ( ErrCode::SUCCESS );
+}
+
 //========================================================================
 //
 //    Public Member Functions.
