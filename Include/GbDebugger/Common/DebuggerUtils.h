045669c12caadf92f37b9ebf764e135c9ef45556
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-04-13 11:53:17 +0900

    空のヘッダファイルを新規作成

---
+++ Include/GbDebugger/Common/DebuggerUtils.h
@@ -0,0 +1,33 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   Gb Debugger Project.   ----                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      Utility Macros/Functions.
+**
+**      @file       Common/DebuggerUtils.h
+**/
+
+#if !defined( GBDEBUGGER_COMMON_INCLUDED_DEBUGGER_UTILS_H )
+#    define   GBDEBUGGER_COMMON_INCLUDED_DEBUGGER_UTILS_H
+
+#if !defined( GBDEBUGGER_COMMON_INCLUDED_DEBUGGER_TYPES_H )
+#    include    "DebuggerTypes.h"
+#endif
+
+
+GBDEBUGGER_NAMESPACE_BEGIN
+
+GBDEBUGGER_NAMESPACE_END
+
+#endif
