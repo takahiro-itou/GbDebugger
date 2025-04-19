8f4a1970eb54ce832b329074d173b2bde266b894
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-04-19 22:48:43 +0900

    空のクラスを新規作成
    (1) ArmALU

---
+++ Lib/GbaMan/CpuArm/ArmALU.h
@@ -0,0 +1,106 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   Gb Debugger Project.   ----                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of ArmALU class.
+**
+**      @file       GbaMan/ArmALU.h
+**/
+
+#if !defined( GBDEBUGGER_GBAMAN_INCLUDED_ARM_ALU_H )
+#    define   GBDEBUGGER_GBAMAN_INCLUDED_ARM_ALU_H
+
+
+GBDEBUGGER_NAMESPACE_BEGIN
+namespace  GbaMan  {
+
+//  クラスの前方宣言。  //
+class   MemoryManager;
+
+
+//========================================================================
+//
+//    ArmALU  class.
+//
+
+class  ArmALU
+{
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+private:
+    typedef     ArmALU          This;
+    ArmALU              (const  This  &);
+    This &  operator =  (const  This  &);
+public:
+    //  テストクラス。  //
+    friend  class   ArmALUTest;
+};
+
+}   //  End of namespace  GbaMan
+GBDEBUGGER_NAMESPACE_END
+
+#endif
