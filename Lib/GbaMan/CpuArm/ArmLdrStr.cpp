36cae82c1692b097d2c069f983157529f006f9d8
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-04-27 13:08:35 +0900

    作業途中

--- Lib/GbaMan/CpuArm/ArmLdrStr.cpp
+++ Lib/GbaMan/CpuArm/ArmLdrStr.cpp
@@ -33,6 +33,56 @@ typedef     GBD_REGPARM     InstExecResult
         Memorymanager & manMem,
         RegType       & cpuFlag);
 
+template  <int I, int P, int U, int B, int BIT21, int SHIFTTYPE, int OP>
+GBD_REGPARM     InstExecResult
+armLdrStrInstruction(
+        const  OpeCode  opeCode,
+        RegPair         cpuRegs[],
+        Memorymanager & manMem,
+        RegType       & cpuFlag)
+{
+    const  int  rd  = (opeCode >> 12) & 0x0F;
+    const  int  rn  = (opeCode >> 16) & 0x0F;
+    int         ofs;
+
+    const   bool    flag_cy = (cpuFlag & CPSR::FLAG_C) ? true : false;
+    bool            fout_cy = flag_cy;
+
+    if ( I == 0 ) {
+        //  オフセットは即値。  //
+        ofs = (opeCode & 0x0FFF);
+    } else {
+        //  オフセットはシフトされたレジスタ。  //
+        OpeCode iRm = (opeCode & 0x0F);
+        RegType vRm = cpuRegs[iRm].dw;
+
+        const int shift = (opeCode >> 7) & 0x1F;
+
+        //  ビット 05..06 はシフトの種類。  //
+        switch ( SHIFTTYPE ) {
+        case  0:    //  LSL
+            ofs = ArmALURmLslImm()(shift, vRm, fout_cy, flag_cy);
+            break;
+        case  1:    //  LSR
+            ofs = ArmALURmLsrImm()(shift, vRm, fout_cy, flag_cy);
+            break;
+        case  2:    //  ASR
+            ofs = ArmALURmAsrImm()(shift, vRm, fout_cy, flag_cy);
+            break;
+        case  3:    //  ROR
+            ofs = ArmALURmRorImm()(shift, vRm, fout_cy, flag_cy);
+            break;
+        }
+    }
+
+    //  U フラグ (BIT 23)   //
+    if ( U == 0 ) {
+        //  オフセットはマイナス。  //
+        ofs = - ofs;
+    }
+
+}
+
 }   //  End of (Unnamed) namespace.
 
 
