0f2b4b6386456e8028f6a42fbf17d44a340087c4
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-04-29 10:59:04 +0900

    ソースファイルを新規作成

---
+++ Lib/GbaMan/CpuArm/ArmBranch.cpp
@@ -0,0 +1,38 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   Gb Debugger Project.   ----                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of Branch Instructions.
+**
+**      @file       GbaMan/ArmBranchcpp
+**/
+
+#include    "CpuArm.h"
+
+
+GBDEBUGGER_NAMESPACE_BEGIN
+namespace  GbaMan  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace.
+
+
+//========================================================================
+//
+//    Branch  Instructions.
+//
+
+}   //  End of namespace  GbaMan
+GBDEBUGGER_NAMESPACE_END
