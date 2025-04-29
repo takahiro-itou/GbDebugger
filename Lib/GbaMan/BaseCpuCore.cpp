ec10cbf3de953796732b1522c04b590c7475f0f8
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-04-29 12:16:20 +0900

    クラスを新規作成

---
+++ Lib/GbaMan/BaseCpuCore.cpp
@@ -0,0 +1,156 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   Gb Debugger Project.   ----                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of BaseCpuCore class.
+**
+**      @file       GbaMan/BaseCpuCore.cpp
+**/
+
+#include    "BaseCpuCore.h"
+
+#include    "GbDebugger/GbaMan/MemoryManager.h"
+
+#include    "GbDebugger/Common/DebuggerUtils.h"
+
+#include    <iostream>
+#include    <ostream>
+
+
+GBDEBUGGER_NAMESPACE_BEGIN
+namespace  GbaMan  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace.
+
+
+//========================================================================
+//
+//    BaseCpuCore  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （コンストラクタ）。
+//
+
+BaseCpuCore::BaseCpuCore(
+        MemoryManager & manMem)
+    : m_manMem(manMem)
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+BaseCpuCore::~BaseCpuCore()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//----------------------------------------------------------------
+//    レジスタをリセットする。
+//
+
+ErrCode
+BaseCpuCore::doHardReset()
+{
+}
+
+//----------------------------------------------------------------
+//    現在の命令を実行する。
+//
+
+InstExecResult
+BaseCpuCore::executeNextInst()
+{
+}
+
+//----------------------------------------------------------------
+//    レジスタの内容をダンプする。
+//
+
+std::ostream  &
+BaseCpuCore::printRegisters(
+        std::ostream  & outStr)  const
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//----------------------------------------------------------------
+//    命令をプリフェッチする。
+//
+
+void
+BaseCpuCore::prefetchAll()
+{
+}
+
+//----------------------------------------------------------------
+//    次の命令をプリフェッチする。
+//
+
+void
+BaseCpuCore::prefetchNext()
+{
+}
+
+}   //  End of namespace  GbaMan
+GBDEBUGGER_NAMESPACE_END
---
+++ Lib/GbaMan/BaseCpuCore.h
@@ -0,0 +1,209 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   Gb Debugger Project.   ----                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of BaseCpuCore class.
+**
+**      @file       GbaMan/BaseCpuCore.h
+**/
+
+#if !defined( GBDEBUGGER_GBAMAN_INCLUDED_BASE_CPU_CORE_H )
+#    define   GBDEBUGGER_GBAMAN_INCLUDED_BASE_CPU_CORE_H
+
+#if !defined( GBDEBUGGER_COMMON_INCLUDED_DEBUGGER_UTILS_H )
+#    include    "GbDebugger/Common/DebuggerUtils.h"
+#endif
+
+#if !defined( GBDEBUGGER_GBAMAN_INCLUDED_CPU_UTILS_H )
+#    include    "GbDebugger/GbaMan/CpuUtils.h"
+#endif
+
+#if !defined( GBDEBUGGER_SYS_STL_INCLUDED_IOSFWD )
+#    include    <iosfwd>
+#    define   GBDEBUGGER_SYS_STL_INCLUDED_IOSFWD
+#endif
+
+
+GBDEBUGGER_NAMESPACE_BEGIN
+namespace  GbaMan  {
+
+//  クラスの前方宣言。  //
+class   MemoryManager;
+
+
+//========================================================================
+//
+//    BaseCpuCore  class.
+//
+
+class  BaseCpuCore
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （コンストラクタ）。
+    **
+    **/
+    BaseCpuCore(
+            MemoryManager & manMem);
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~BaseCpuCore();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   レジスタをリセットする。
+    **
+    **  @return     エラーコードを返す。
+    **      -   異常終了の場合は、
+    **          エラーの種類を示す非ゼロ値を返す。
+    **      -   正常終了の場合は、ゼロを返す。
+    **/
+    virtual  ErrCode
+    doHardReset();
+
+    //----------------------------------------------------------------
+    /**   現在の命令を実行する。
+    **
+    **/
+    virtual  InstExecResult
+    executeNextInst();
+
+    //----------------------------------------------------------------
+    /**   レジスタの内容をダンプする。
+    **
+    **/
+    virtual  std::ostream  &
+    printRegisters(
+            std::ostream  & outStr)  const;
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   プログラムカウンタを取得する。
+    **
+    **/
+    const   GuestMemoryAddress
+    getNextPC()  const
+    {
+        return ( this->m_nextPC );
+    }
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+private:
+
+    //----------------------------------------------------------------
+    /**   命令をプリフェッチする。
+    **
+    **/
+    void
+    prefetchAll();
+
+    //----------------------------------------------------------------
+    /**   次の命令をプリフェッチする。
+    **
+    **/
+    void
+    prefetchNext();
+
+//========================================================================
+//
+//    Member Variables.
+//
+private:
+
+    /**   メモリマネージャ。    **/
+    MemoryManager  &        m_manMem;
+
+    /**   レジスタ。            **/
+    RegPair                 m_cpuRegs[85];
+
+    /**   次の命令のアドレス。  **/
+    GuestMemoryAddress      m_nextPC;
+
+    /**   プリフェッチ。        **/
+    OpeCode                 m_prefOpeCodes[2];
+
+    /**   現在のモード。        **/
+    RegType                 m_thumbState;
+
+//========================================================================
+//
+//    Other Features.
+//
+private:
+    typedef     BaseCpuCore     This;
+    BaseCpuCore         (const  This  &);
+    This &  operator =  (const  This  &);
+public:
+    //  テストクラス。  //
+    friend  class   BaseCpuCoreTest;
+};
+
+}   //  End of namespace  GbaMan
+GBDEBUGGER_NAMESPACE_END
+
+#endif
