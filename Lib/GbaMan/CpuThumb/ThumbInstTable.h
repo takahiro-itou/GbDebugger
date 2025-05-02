6e6c5fb58d0613d114309d7b43c5ac420fd8ce18
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-05-02 21:33:13 +0900

    ヘッダファイルを新規作成

---
+++ Lib/GbaMan/CpuThumb/ThumbInstTable.h
@@ -0,0 +1,24 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   Gb Debugger Project.   ----                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of Thumb Instruction Table.
+**
+**      @file       GbaMan/ThumbInst.h
+**/
+
+#if !defined( GBDEBUGGER_GBAMAN_INCLUDED_THUMB_INST_H )
+#    define   GBDEBUGGER_GBAMAN_INCLUDED_THUMB_INST_H
+
+#endif
