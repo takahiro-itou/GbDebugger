02496703e6790b844eae5afa9aaaa4f602d54210
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-05-03 23:51:44 +0900

    空のヘッダファイルを新規作成

---
+++ Lib/GbaMan/Utils/ShiftOperator.h
@@ -0,0 +1,38 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   Gb Debugger Project.   ----                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of ShiftOperators.
+**
+**      @file       GbaMan/Utils/ShiftOperator.h
+**/
+
+#if !defined( GBDEBUGGER_GBAMAN_UTILS_INCLUDED_SHIFT_OPERATOR_H )
+#    define   GBDEBUGGER_GBAMAN_UTILS_INCLUDED_SHIFT_OPERATOR_H
+
+#if !defined( GBDEBUGGER_GBAMAN_INCLUDED_CPU_UTILS_H )
+#    include    "GbDebugger/GbaMan/CpuUtils.h"
+#endif
+
+
+GBDEBUGGER_NAMESPACE_BEGIN
+namespace  GbaMan  {
+
+//  クラスの前方宣言。  //
+
+
+}   //  End of namespace  GbaMan
+GBDEBUGGER_NAMESPACE_END
+
+#endif
