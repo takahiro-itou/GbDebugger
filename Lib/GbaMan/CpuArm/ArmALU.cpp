477e4d80c484c9cd22ebcb49a11d93f7f3511c8d
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-05-05 19:09:10 +0900

    リファクタリング

--- Lib/GbaMan/CpuArm/ArmALU.cpp
+++ Lib/GbaMan/CpuArm/ArmALU.cpp
@@ -61,22 +61,20 @@ armALUInstruction(
 
     //  第一オペランドレジスタはビット 16..19 で指定。  //
     const  RegType  lhs = cpuRegs[(opeCode >> 16) & 0x0F].dw;
-    RegType         rhs;
-    uint64_t        res;
-
-    const   RegType Cy      = (cpuFlag >> CPSR::FBIT_C) & 0x01;
-    const   bool    flag_cy = (Cy ? true : false);
-    bool            fout_cy = flag_cy;
+    const  RegType  Cy  = (cpuFlag >> CPSR::FBIT_C) & 0x01;
+    RegType     rhs;
+    uint64_t    res;
+    bool        flagCy = (Cy ? true : false);
 
     if ( BIT25 == 0 ) {
         //  第二オペランドはレジスタ。ビット 00..07 で指定される。  //
         rhs = getAluOp2Register<SHIFTTYPE, BIT4>(
-                opeCode, cpuRegs, fout_cy);
+                opeCode, cpuRegs, flagCy);
     } else {
         //  第二オペランドは即値指定。ビット 00..07 で指定される。  //
         const  RegType  imm = (opeCode & 0xFF);
         const  int      ror = (opeCode & 0xF00) >> 7;
-        rhs = ArmALUImmRor()(imm, ror, fout_cy);
+        rhs = ArmALUImmRor()(imm, ror, flagCy);
     }
 
     const  RegType  cur = cpuFlag;
@@ -85,81 +83,81 @@ armALUInstruction(
     switch ( CODE ) {
     case  0x00:     //  AND         Rd = Rn AND Op2
         res = lhs & rhs;
-        flg = setCondLogical(res, lhs, rhs, fout_cy, cur);
+        flg = setCondLogical(res, lhs, rhs, flagCy, cur);
         cpuRegs[dst].dw = res;
         break;
     case  0x01:     //  EOR (XOR)   Rd = Rn XOR Op2
         res = lhs ^ rhs;
-        flg = setCondLogical(res, lhs, rhs, fout_cy, cur);
+        flg = setCondLogical(res, lhs, rhs, flagCy, cur);
         cpuRegs[dst].dw = res;
         break;
     case  0x02:     //  SUB         Rd = Rn - OP2
         res = static_cast<uint64_t>(lhs) - static_cast<uint64_t>(rhs);
-        flg = setCondSub(res, lhs, rhs, fout_cy, cur);
+        flg = setCondSub(res, lhs, rhs, flagCy, cur);
         cpuRegs[dst].dw = res;
         break;
     case  0x03:     //  RSB         Rd = Op2 - Rn
         res = static_cast<uint64_t>(rhs) - static_cast<uint64_t>(lhs);
-        flg = setCondSub(res, rhs, lhs, fout_cy, cur);
+        flg = setCondSub(res, rhs, lhs, flagCy, cur);
         cpuRegs[dst].dw = res;
         break;
     case  0x04:     //  ADD         Rd = Rn + Op2
         res = static_cast<uint64_t>(lhs) + static_cast<uint64_t>(rhs);
-        flg = setCondAdd(res, lhs, rhs, fout_cy, cur);
+        flg = setCondAdd(res, lhs, rhs, flagCy, cur);
         cpuRegs[dst].dw = res;
         break;
     case  0x05:     //  ADC         Rd = Rn + Op2 + Cy
         res = static_cast<uint64_t>(lhs) + static_cast<uint64_t>(rhs)
                     + static_cast<uint64_t>(Cy);
-        flg = setCondAdd(res, lhs, rhs, fout_cy, cur);
+        flg = setCondAdd(res, lhs, rhs, flagCy, cur);
         cpuRegs[dst].dw = res;
         break;
     case  0x06:     //  SBC         Rd = Rn - Op2 + Cy - 1
         res = static_cast<uint64_t>(lhs) - static_cast<uint64_t>(rhs)
                     + static_cast<uint64_t>(Cy - 1);
-        flg = setCondAdd(res, lhs, rhs, fout_cy, cur);
+        flg = setCondAdd(res, lhs, rhs, flagCy, cur);
         cpuRegs[dst].dw = res;
         break;
     case  0x07:     //  RSC         Rd = Op2 - Rn + Cy - 1
         res = static_cast<uint64_t>(rhs) - static_cast<uint64_t>(lhs)
                     + static_cast<uint64_t>(Cy - 1);
-        flg = setCondAdd(res, rhs, lhs, fout_cy, cur);
+        flg = setCondAdd(res, rhs, lhs, flagCy, cur);
         cpuRegs[dst].dw = res;
         break;
     case  0x08:     //  TST         (void)(Rn AND Op2)
         res = lhs & rhs;
-        flg = setCondLogical(res, lhs, rhs, fout_cy, cur);
+        flg = setCondLogical(res, lhs, rhs, flagCy, cur);
         break;
     case  0x09:     //  TEQ         (void)(Rn XOR Op2)
         res = lhs ^ rhs;
-        flg = setCondLogical(res, lhs, rhs, fout_cy, cur);
+        flg = setCondLogical(res, lhs, rhs, flagCy, cur);
         break;
     case  0x0A:     //  CMP         (void)(Rn - Op2)
         res = static_cast<uint64_t>(lhs) - static_cast<uint64_t>(rhs);
-        flg = setCondSub(res, lhs, rhs, fout_cy, cur);
+        flg = setCondSub(res, lhs, rhs, flagCy, cur);
         break;
     case  0x0B:     //  CMN         (void)(Rn + Op2)
         res = static_cast<uint64_t>(lhs) + static_cast<uint64_t>(rhs);
-        flg = setCondAdd(res, lhs, rhs, fout_cy, cur);
+        flg = setCondAdd(res, lhs, rhs, flagCy, cur);
         break;
     case  0x0C:     //  ORR (OR)    Rd = Rn OR Op2
         res = lhs | rhs;
-        flg = setCondLogical(res, lhs, rhs, fout_cy, cur);
+        flg = setCondLogical(res, lhs, rhs, flagCy, cur);
         cpuRegs[dst].dw = res;
         break;
     case  0x0D:     //  MOV         Rd = Op2
         res = rhs;
-        flg = setCondLogical(res, lhs, rhs, fout_cy, cur);
+        flg = setCondLogical(res, lhs, rhs, flagCy, cur);
         cpuRegs[dst].dw = res;
         break;
     case  0x0E:     //  BIC         Rd = Rnn AND NOT Op2
         res = lhs & ~rhs;
-        flg = setCondLogical(res, lhs, rhs, fout_cy, cur);
+        flg = setCondLogical(res, lhs, rhs, flagCy, cur);
         cpuRegs[dst].dw = res;
         break;
     case  0x0F:     //  MVN         Rd = Not Op2
         res = ~rhs;
-        flg = setCondLogical(res, lhs, rhs, fout_cy, cur);
+        flg = setCondLogical(res, lhs, rhs, flagCy, cur);
         cpuRegs[dst].dw = res;
         break;
     }
