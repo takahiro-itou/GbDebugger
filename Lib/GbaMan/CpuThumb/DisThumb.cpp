16dac20eed807887e2b4394c424f3f687d324e54
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-05-07 20:07:03 +0900

    ニーモニック表示を更新

--- Lib/GbaMan/CpuThumb/DisThumb.cpp
+++ Lib/GbaMan/CpuThumb/DisThumb.cpp
@@ -31,9 +31,9 @@ namespace  {
 CONSTEXPR_VAR   const  MnemonicMap
 thumbMnemonics[] = {
     //  Format 01 : ビットシフト。  //
-    { 0xF800, 0x0000, "LSL\t%r0, %r3, #%I{6,31}" },
-    { 0xF800, 0x0800, "LSR" },
-    { 0xF800, 0x1000, "ASR" },
+    { 0xF800, 0x0000, "LSL \t%r0, %r3, #%I{6,31}" },
+    { 0xF800, 0x0800, "LSR \t%r0, %r3, #%I{6,31}" },
+    { 0xF800, 0x1000, "ASR \t%r0, %r3, #%I{6,31}" },
 
     //  Format 02 : 加算減算。      //
     { 0xFE00, 0x1800, "ADD" },
