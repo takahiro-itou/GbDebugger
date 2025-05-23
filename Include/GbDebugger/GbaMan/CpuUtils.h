57f2b2c062c19f7e702522bac4d90c864080aa82
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-05-23 18:21:33 +0900

    定数定義を追加

--- Include/GbDebugger/GbaMan/CpuUtils.h
+++ Include/GbDebugger/GbaMan/CpuUtils.h
@@ -59,6 +59,29 @@ enum class  InstExecResult
     SUCCESS_BREAKPOINT  = 2,
 };
 
+/**
+**    演算の種類。
+**/
+
+enum class  AluOp  {
+    AND = 0x00,
+    EOR = 0x01,
+    SUB = 0x02,
+    RSB = 0x03,
+    ADD = 0x04,
+    ADC = 0x05,
+    SBC = 0x06,
+    RSC = 0x07,
+    TST = 0x08,
+    TEQ = 0x09,
+    CMP = 0x0A,
+    CMN = 0x0B,
+    ORR = 0x0C,
+    MOV = 0x0D,
+    BIC = 0x0E,
+    MVN = 0x0F,
+};
+
 
 //========================================================================
 //
